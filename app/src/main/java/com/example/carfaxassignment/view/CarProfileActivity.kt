7abcb1141a0f57aa62ab2bbe83124d6b56bcaa9b
Anju Thamor <anju.thamor@gmail.com>
2021-02-14 22:47:18 -0500

    *Designed CarProfile screen

--- app/src/main/java/com/example/carfaxassignment/view/CarProfileActivity.kt
+++ app/src/main/java/com/example/carfaxassignment/view/CarProfileActivity.kt
@@ -1,6 +1,11 @@
 package com.example.carfaxassignment.view
 
+import android.content.Intent
+import android.graphics.Color
+import android.net.Uri
 import android.os.Bundle
+import android.view.View
+import android.view.WindowManager
 import androidx.appcompat.app.AppCompatActivity
 import com.bumptech.glide.Glide
 import com.example.carfaxassignment.R
@@ -14,6 +19,12 @@ class CarProfileActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_carprofile)
+        window.apply {
+            clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS)
+            addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
+            decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+            statusBarColor =resources.getColor(R.color.transgrey)
+        }
         if (intent.hasExtra(IntentKeys.CAR_ITEM)) {
             carItem =
                 intent.getSerializableExtra(IntentKeys.CAR_ITEM) as Car?
@@ -28,9 +39,23 @@ class CarProfileActivity : AppCompatActivity() {
             .load(car.images?.firstPhoto?.large)
             .into(CarimageView)
 
-        yearText.text=car.year
-        makeTextview.text=car.make
-        modelTextview.text=car.model
+        cartitles.text=  car.year + " " + car.make + " " +
+                car.model + " " + car.trim
+        carotherdetails.text= "$ " + car.currentPrice + "  |  " + car.mileage + " k mi "
+        carinfolocation.text= car.dealer?.city + "," + car.dealer?.state
+
+        carinfoExterior.text=car.exteriorColor
+        carinfoInterior.text=car.interiorColor
+        carinfoDrive.text=car.drivetype
+        carinfoTransmission.text=car.transmission
+        carinfoBody.text=car.bodytype
+        carinfoEngine.text=car.engine
+        carinfoFuel.text=car.fuel
+
+        callButton.setOnClickListener {
+            val intent = Intent(Intent.ACTION_DIAL, Uri.parse("tel:${car.dealer?.phone}"))
+            startActivity(intent)
+        }
     }
 
 }
